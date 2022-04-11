c405dbddd96835de62be6f066f8676a13fd838d1
Ana Ramos <ana.ramos.neira@gmail.com>
2022-04-11 15:50:54 -0500

    Nombres a las etiquetas Modelo, Precio, Ancho.

--- src/cibertec/ModificarMaleta.java
+++ src/cibertec/ModificarMaleta.java
@@ -47,56 +47,56 @@ public class ModificarMaleta extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(22, 40, 46, 14);
+		JLabel lblNewLabel = new JLabel("Modelo");
+		lblNewLabel.setBounds(22, 40, 86, 14);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(22, 76, 46, 14);
+		JLabel lblNewLabel_1 = new JLabel("Precio (S/)");
+		lblNewLabel_1.setBounds(22, 76, 104, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
-		lblNewLabel_2.setBounds(22, 117, 46, 14);
+		JLabel lblNewLabel_2 = new JLabel("Ancho (cm)");
+		lblNewLabel_2.setBounds(22, 117, 104, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
-		lblNewLabel_3.setBounds(22, 156, 46, 14);
+		JLabel lblNewLabel_3 = new JLabel("Alto (cm)");
+		lblNewLabel_3.setBounds(22, 156, 115, 14);
 		contentPane.add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
-		lblNewLabel_4.setBounds(22, 194, 46, 14);
+		JLabel lblNewLabel_4 = new JLabel("Fondo (cm)");
+		lblNewLabel_4.setBounds(22, 194, 104, 14);
 		contentPane.add(lblNewLabel_4);
 		
 		textField = new JTextField();
-		textField.setBounds(185, 73, 86, 20);
+		textField.setBounds(118, 73, 153, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(185, 114, 86, 20);
+		textField_1.setBounds(118, 114, 153, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(185, 153, 86, 20);
+		textField_2.setBounds(118, 153, 153, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(185, 191, 86, 20);
+		textField_3.setBounds(118, 191, 153, 20);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("Cerrar");
 		btnNewButton.setBounds(315, 36, 89, 23);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("New button");
+		JButton btnNewButton_1 = new JButton("Grabar");
 		btnNewButton_1.setBounds(315, 98, 89, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(156, 36, 115, 22);
+		comboBox.setBounds(118, 36, 153, 22);
 		contentPane.add(comboBox);
 	}
 }
